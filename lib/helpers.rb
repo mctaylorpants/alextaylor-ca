c1931285f879ddc49490fedf9e2ed4df2b76a82a
Alex Taylor <alex@alextaylor.ca>
2023-08-18 12:04:08 -0700

    Add previous/next article links

--- lib/helpers.rb
+++ lib/helpers.rb
@@ -1,3 +1,13 @@
 use_helper Nanoc::Helpers::Blogging
 use_helper Nanoc::Helpers::LinkTo
 use_helper Nanoc::Helpers::Text
+
+def article_after(current_item)
+  index = sorted_articles.find_index(current_item)&.pred
+  sorted_articles[index] if index && index >= 0
+end
+
+def article_before(current_item)
+  index = sorted_articles.find_index(current_item)&.succ
+  sorted_articles[index] if index
+end
